6bff595dd5f6a4d6283f901275838ed274e0e08d
fernandohernandezpaz <fernandohernandezpaz@outlook.com>
2022-08-17 11:29:46 -0600

    feat: custom validations for fields

---
+++ src/helpers/db-validator.js
@@ -0,0 +1,38 @@
+const RolModel = require('../models/rol.model');
+const UsuarioModel = require('../models/usuario.model');
+
+
+const validRol = async (nombre = '') => {
+    const existeRol = await RolModel.findOne({
+        nombre
+    });
+
+    if (!existeRol) {
+        throw new Error(`El rol ${nombre} no esta registrado`)
+    }
+}
+
+
+const existsEmail = async (correo = '') => {
+    const existeEmail = await UsuarioModel.findOne({
+        correo
+    });
+
+    if (existeEmail) {
+        throw new Error(`El correo ${correo} ya existe`);
+    }
+}
+
+const existsUser = async (id) => {
+    const existeUsuario = await UsuarioModel.findById(id);
+    if (!existeUsuario) {
+        throw new Error(`El id no existe ${id}`);
+    }
+}
+
+
+module.exports = {
+    validRol,
+    existsEmail,
+    existsUser
+}
